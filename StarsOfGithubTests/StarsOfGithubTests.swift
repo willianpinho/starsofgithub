4b096b56543b287bf6238eb6c68e4ab110acc12d
Willian Pinho <willianpinho@gmail.com>
2019-11-21 15:14:11 -0300

    Initial Commit

---
+++ StarsOfGithubTests/StarsOfGithubTests.swift
@@ -0,0 +1,34 @@
+//
+//  StarsOfGithubTests.swift
+//  StarsOfGithubTests
+//
+//  Created by Willian Pinho on 21/11/19.
+//  Copyright © 2019 Willian Pinho. All rights reserved.
+//
+
+import XCTest
+@testable import StarsOfGithub
+
+class StarsOfGithubTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
